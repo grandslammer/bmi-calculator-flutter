264ebe40e057783e4091a8a0e08f48d4fb29e902
Ivan <ivanwhitemac@gmail.com>
2020-01-16 17:22:29 +0000

    Male and female button has colour change functionality and is refactored

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,7 +6,10 @@ import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xff1d1e33);
+const inactiveCardColour = Color(0xff111328);
 const bottomContainerColour = Color(0xffeb1555);
+
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   @override
@@ -14,6 +17,8 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,16 +32,25 @@ class _InputPageState extends State<InputPage> {
                 children: <Widget>[
                   Expanded(
                     child: ReusableCard(
-                      colour: activeCardColour,
-                      cardChild: IconContent(
-                          icon: FontAwesomeIcons.mars, label: 'MALE'),
+                      onPress: () {
+                        setState(() {
+                          selectedGender = Gender.male;
+                        });
+                      },
+                      colour: selectedGender == Gender.male ? activeCardColour : inactiveCardColour,
+                      cardChild: IconContent(icon: FontAwesomeIcons.mars, label: 'MALE'),
                     ),
                   ),
                   Expanded(
                     child: ReusableCard(
-                      colour: activeCardColour,
-                      cardChild: IconContent(
-                          icon: FontAwesomeIcons.venus, label: 'FEMALE'),
+                      onPress: () {
+                        setState(() {
+                          selectedGender = Gender.female;
+                        });
+                      },
+                      colour:
+                          selectedGender == Gender.female ? activeCardColour : inactiveCardColour,
+                      cardChild: IconContent(icon: FontAwesomeIcons.venus, label: 'FEMALE'),
                     ),
                   ),
                 ],
